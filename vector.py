fe4ac8050d158bf6da85a22404ca2f67b8af11bb
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2018-07-02 17:53:45 +0400

    Added cross product

--- vector.py
+++ vector.py
@@ -40,7 +40,7 @@ class Vector():
     
     # Magnitude and Direction
     def magnitude(self):
-        coordinates_squared = [x**2 for x in v.coordinates]
+        coordinates_squared = [x**2 for x in self.coordinates]
         return Decimal(math.sqrt(sum(coordinates_squared)))
     
     def normalized(self):
@@ -99,6 +99,34 @@ class Vector():
                 raise Exception("No unique orthogonal component")
             else:
                 raise e
+    
+    def cross(self, v):
+        try:
+            x_1, y_1, z_1 = self.coordinates
+            x_2, y_2, z_2 = v.coordinates
+            new_coordinates = [y_1*z_2 - y_2*z_1,
+                               -(x_1*z_2 - x_2*z_1),
+                               x_1*y_2 - x_2*y_1 ]
+            return Vector(new_coordinates)
+
+        except ValueError as e:
+            msg = str(e)
+            if msg == "need more than 2 values to unpack":
+                self_embedded_R3 = Vector(self.coordinates + ('0',))
+                v_embedded_R3 = Vector(v.coordinates + ('0',))
+                return self_embedded_R3.cross(v_embedded_R3)
+            elif (msg == "too many values to unpack" or
+                  msg == "need more than 1 value to unpack"):
+                raise Exception("Only defined in two, three dimensions")
+            else:
+                raise e
+    
+    def area_of_triangle_with(self, v):
+        return self.area_of_parallelogram_with(v) / Decimal('2.0')
+    
+    def area_of_parallelogram_with(self, v):
+        cross_product = self.cross(v)
+        return cross_product.magnitude()
         
 
 if __name__ == '__main__':
@@ -171,4 +199,17 @@ if __name__ == '__main__':
    # Component paralel / orthogonal
    v = Vector([3.039, 1.879])
    w = Vector([0.825, 2.036])
-   print(v.component_parallel_to(w)) 
+   print(v.component_parallel_to(w))
+
+   # Cross product
+   v = Vector([8.462, 7.893, -8.187])
+   w = Vector([6.984, -5.975, 4.778])
+   print(v.cross(w))
+
+   v = Vector([-8.987, -9.838, 5.031])
+   w = Vector([-4.268, -1.861, -8.866])
+   print(v.area_of_parallelogram_with(w))
+
+   v = Vector([1.5, 9.547, 3.691])
+   w = Vector([-6.007, 0.124, 5.772])
+   print(v.area_of_triangle_with(w)) 
